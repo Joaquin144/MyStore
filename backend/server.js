312003e0e7dd6928fe6038ba6b74f460c002273a
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-16 13:09:40 +0530

    error location changed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,39 @@
 const app = require('./app');
-const dotenv = require("dotenv");
 
-//Config
-dotenv.config({ path: "backend/config/config.env" });
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is listening at PORT: ${process.env.PORT}`);
+app.listen(3000,()=>{
+    console.log(`Server is listening at PORT: ${3000}`);
 });
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// app.listen(3000,()=>{
+//     console.log(`Server is listening at PORT: ${3000}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = require('./app');
+// const dotenv = require("dotenv");
+
+// //Config
+// dotenv.config({ path: "backend/config/config.env" });
+
+
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`Server is listening at PORT: ${process.env.PORT}`);
+// });
