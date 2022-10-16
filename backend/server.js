aa384cf1d99001e513ce386d0c31e562d7800cc5
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-16 13:12:40 +0530

    big issue

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-const app = require('./app');
+const app = require("./app")
+
+//const app = require('./app');
 const dotenv = require("dotenv");
 
 //Config
 dotenv.config({ path: "backend/config/config.env" });
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is listening at PORT: ${process.env.PORT}`);
+app.listen(4000, ()=>{
+    console.log(`Server is listening at PORT: 4000`);
 });
+
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`Server is listening at PORT: ${process.env.PORT}`);
+// });
