6f17ee64876dad78d61d184525579c5e3eb38437
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-16 13:33:53 +0530

    all okay nice ;)

--- backend/server.js
+++ backend/server.js
@@ -8,37 +8,3 @@ dotenv.config({ path: "backend/config/config.env" });
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening at PORT: ${process.env.PORT}`);
 });
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-
-// app.listen(3000,()=>{
-//     console.log(`Server is listening at PORT: ${3000}`);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = require('./app');
-// const dotenv = require("dotenv");
-
-// //Config
-// dotenv.config({ path: "backend/config/config.env" });
-
-
-// app.listen(process.env.PORT, ()=>{
-//     console.log(`Server is listening at PORT: ${process.env.PORT}`);
-// });
