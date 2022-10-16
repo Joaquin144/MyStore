0c7808ebf944813786285148dd683ac2c4618001
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-16 15:13:01 +0530

    error in connecting mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require('./app');
-
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
 
 //Config
 dotenv.config({ path: "backend/config/config.env" });
+
+//Connecting to DB
+connectDatabase();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening at PORT: ${process.env.PORT}`);
