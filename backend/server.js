b6a97fbfd8bb8662132fd88fe7d0608c9404752f
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-16 13:22:35 +0530

    same echo cmd problem with config.env: Resolved

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
 
-app.listen(3000,()=>{
-    console.log(`Server is listening at PORT: ${3000}`);
+const dotenv = require("dotenv");
+
+//Config
+dotenv.config({ path: "backend/config/config.env" });
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listening at PORT: ${process.env.PORT}`);
 });
 
 
