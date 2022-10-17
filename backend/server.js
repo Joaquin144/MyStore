d22c4b22c1203bbe184a8eb0207e1ed570a53527
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-17 15:19:38 +0530

    Uncaught Exception

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require('./app');
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database")
 
+//Hanlding Uncaught Exception --> eg: When we use a varibale that was not defined
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down server due to Uncaught Exception`);
+    process.exit(1);
+});
+
 //Config
 dotenv.config({ path: "backend/config/config.env" });
 
