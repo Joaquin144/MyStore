9b1180115b6fb164f700848afded2c0285a43e74
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-17 15:16:36 +0530

    Unhandled promise rejection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = ()=>{
-    mongoose.connect("mongodb://localhost:27017/ecomm",{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
         console.log(`MongoDB connected with server ${data.connection.host}`);
-    }).catch((err)=>{
-        console.log(err);
-    });
+    })
 }
 
 module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,17 @@ dotenv.config({ path: "backend/config/config.env" });
 //Connecting to DB
 connectDatabase();
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server is listening at PORT: ${process.env.PORT}`);
 });
+
+//Unhandled Promise Rejection --> eg. occurs when mongodb url is wrong
+//Objective: In such kinds of errors we should shut down server quickly
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down server due to Unhandled Promise Rejection`);
+    
+    server.close(()=>{
+        process.exit(1);
+    });
+})
