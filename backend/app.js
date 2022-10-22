bde6b92257bdbd65ab69734ff8a6d3ee429717ce
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-22 11:16:56 +0530

    order APIs done

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 //Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
+const order = require("./routes/orderRoute");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/v1",product);
 app.use("/api/v1",user);
+app.use("/api/v1",order);
 
 //MiddleWare for error
 app.use(errorMiddleware);
